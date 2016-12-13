9c925ef7b1c85a6e0f822734814963ab5fad6232
Brad Vogel <brad@mixmax.com>
2016-12-12 19:35:16 -0800

    Remove extra plugins that aren't needed.

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,11 @@
 /* jshint node:true */
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import rootImport from 'rollup-plugin-root-import';
 
 var pkg = require('./package.json');
 
 export default {
   entry: 'src/index.js',
   plugins: [
-    rootImport({
-      root: `${__dirname}/src/common`,
-      useEntry: 'prepend',
-      // Because we omit the .js most of the time, we put it first, and explicitly specify that it
-      // should attempt the lack of extension only after it tries to resolve with the extension.
-      extensions: ['.js', '']
-    }),
-    nodeResolve(),
-    commonjs({
-      include: ['node_modules/**', 'src/common/**'],
-      // laze fails https://github.com/rollup/rollup/wiki/Troubleshooting#name-is-not-exported-by-module
-      // for some reason.
-      namedExports: {
-        'laze': ['defineAll']
-      }
-    }),
     babel({
       presets: [
         [
